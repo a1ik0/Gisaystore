8012920c3689336cf5e914e27bc50d77a5c04ad6
Ali Guliyev <120979675+a1ik0@users.noreply.github.com>
2023-02-21 18:29:22 +0100

    static changes

--- gisaycreates/gisaycreates/settings.py
+++ gisaycreates/gisaycreates/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/admin/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
